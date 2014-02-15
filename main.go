4b9879ea6d9a2327abb19473e19e7b1db462a4a0
Shawn Webb <lattera@gmail.com>
2014-02-15 00:15:41 -0500

    Fix year in license

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 /*
 (BSD 2-clause license)
 
-Copyright (c) 2011-2014, Shawn Webb
+Copyright (c) 2014, Shawn Webb
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
